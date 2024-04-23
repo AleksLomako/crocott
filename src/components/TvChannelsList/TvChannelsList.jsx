e5f2dea944e6946af0a87d51c4834ba1c4c66736
Sasha Lomako <aleks.lomako@yandex.ru>
2024-04-23 11:46:11 +0300

    add structure and components

--- src/components/TvChannelsList/TvChannelsList.jsx
+++ src/components/TvChannelsList/TvChannelsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './TvChannelsList.css';
 import TvChannelCard from '../TvChannelCard/TvChannelCard';
 import content from '../../utils/content';
